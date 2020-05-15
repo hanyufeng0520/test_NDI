3964fe677977da2f7ccefb6430b5e7065aa611a2
j.liu@id4.tv <j.liu@id4.tv>
2020-05-15 12:21:30 +0800

    CREATE

---
+++ test_NDI/test_NDI.cpp
@@ -0,0 +1,12 @@
+﻿// test_NDI.cpp : Defines the entry point for the application.
+//
+
+#include "test_NDI.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
